8b34ac2c629aef5d9c6862a5c74123bd099bae38
Rob Lanphier <robla@robla.net>
2010-03-13 22:02:43 -0800

    getting ready for 0.1.4 release

--- jsonwidget/__init__.py
+++ jsonwidget/__init__.py
@@ -28,7 +28,7 @@ Website: http://robla.net/jsonwidget
 import jsonwidget.termedit
 import os
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 def run_editor(jsonfile, schemafile=None, program_name="jsonwidget " + 
                __version__):
